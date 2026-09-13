06d463d73b17f57f04260093e3910da77f659100
Devhasnain <hasnainalam1166@gmail.com>
2026-09-13 19:21:46 +0500

    #feat added microsoft clarity script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import "@/styles/globals.css";
 
+import { MicrosoftClarity } from "@/components/MicrosoftClarity";
 import RootLayout from "@/components/layout";
 import type { AppProps } from "next/app";
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
+    <>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+      <MicrosoftClarity />
+    </>
   );
 }
